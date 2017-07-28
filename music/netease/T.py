7f46cf82a6e502dc82871f604f807b1b03d20fa0
13969 <1396912801@qq.com>
2017-07-28 13:38:52 +0800

    l来个链接

--- music/netease/T.py
+++ music/netease/T.py
@@ -1,14 +1,15 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
-
-from music.netease import api
-from music.ui import MyWidget, components
-
-from music.ui.untitled import Ui_Form
+from PyQt5.QtCore import QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
-w = components.PlayBar()
+# w = components.PlayBar()
 # w = MyWidget.ProcessBar()
-w.show()
+# w.show()
+player = QMediaPlayer()
+player.setMedia(QMediaContent(QUrl.fromLocalFile("http://m10.music.126.net/20170728134637/4626c99cf08ce937cf64871fc6b33b77/ymusic/6e01/a4d4/bbef/2dda07904eb54d44abb278165e1c6ead.mp3")))
+player.play()
+
 sys.exit(app.exec_())
